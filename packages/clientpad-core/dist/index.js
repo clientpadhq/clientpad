c1d5121d334c7c4cd8669627bab120433cf4b95c
Abdulmuiz Adeyemo <abdulmuizproject@gmail.com>
2026-05-08 11:15:11 +0100

    Fix clientpad core package manifest

--- packages/clientpad-core/dist/index.js
+++ packages/clientpad-core/dist/index.js
@@ -1,2 +1,10 @@
+export const CLIENTPAD_PACKAGE_NAME = "@abdulmuiz44/clientpad-core";
+export const CLIENTPAD_APP_NAME = "ClientPad";
+export function getClientPadPackageInfo() {
+    return {
+        name: CLIENTPAD_PACKAGE_NAME,
+        appName: CLIENTPAD_APP_NAME,
+    };
+}
 export const CLIENTPAD_CORE_PACKAGE_NAME = "@abdulmuiz44/clientpad-core";
 export const CLIENTPAD_CORE_VERSION = "0.1.0";
