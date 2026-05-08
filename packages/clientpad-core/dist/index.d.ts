6bc8d6f1def2ca55b092e0c2350ee16a9f76d6bb
Abdulmuiz Adeyemo <abdulmuizproject@gmail.com>
2026-05-08 11:11:50 +0100

    Limit clientpad core exports

--- packages/clientpad-core/dist/index.d.ts
+++ packages/clientpad-core/dist/index.d.ts
@@ -1,2 +1,7 @@
 export declare const CLIENTPAD_CORE_PACKAGE_NAME = "@abdulmuiz44/clientpad-core";
-export declare const CLIENTPAD_CORE_VERSION = "0.1.0";
+export declare const CLIENTPAD_APP_NAME = "ClientPad";
+export type ClientPadCoreInfo = {
+    packageName: string;
+    appName: string;
+};
+export declare function getClientPadCoreInfo(): ClientPadCoreInfo;
--- packages/clientpad-core/dist/index.js
+++ packages/clientpad-core/dist/index.js
@@ -1,2 +1,8 @@
 export const CLIENTPAD_CORE_PACKAGE_NAME = "@abdulmuiz44/clientpad-core";
-export const CLIENTPAD_CORE_VERSION = "0.1.0";
+export const CLIENTPAD_APP_NAME = "ClientPad";
+export function getClientPadCoreInfo() {
+    return {
+        packageName: CLIENTPAD_CORE_PACKAGE_NAME,
+        appName: CLIENTPAD_APP_NAME,
+    };
+}
